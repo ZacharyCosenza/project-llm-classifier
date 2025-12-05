9963f2065a104ef4fed5ab81addfa9bb946ffb0e
ZacharyCosenza <cosenzazachary@gmail.com>
2025-12-05 13:44:42 -0500

    mod startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,16 @@
 sudo apt update
 sudo apt install python3.11 python3.11-venv python3.11-distutils -y
+
 python3.11 -m venv .venv
 source .venv/bin/activate
+
 python -m pip install --upgrade pip
-pip install numpy
-pip install matplotlib
-pip install scikit-learn
-pip install torch
-pip install huggingface
-pip install kaggle
+pip install numpy pandas matplotlib scikit-learn torch huggingface transformers kaggle
+
 export KAGGLE_API_TOKEN=KGAT_47fa0afab6edd8f9a7856ed3ae7a05c8
 kaggle competitions download -c llm-classification-finetuning
+
+# --- unzip latest download into ./data ---
+latest_zip=$(ls -t *.zip | head -n 1)
+unzip "$latest_zip" -d data
+rm "$latest_zip"
