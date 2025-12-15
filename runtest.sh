f542cb1b35a1286b959b80b92e0945dad0e7121c
ZacharyCosenza <cosenzazachary@gmail.com>
2025-12-15 13:10:12 -0500

    deberta fix

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,5 @@
 
 python run_5_models.py --smoke --base_model roberta-base
 python run_5_models.py --smoke --base_model electra-base-discriminator
-python run_5_models.py --smoke --base_model deberta-base
+python run_5_models.py --smoke --base_model microsoft/deberta-base
 python run_5_models.py --smoke --base_model albert-base-v2
